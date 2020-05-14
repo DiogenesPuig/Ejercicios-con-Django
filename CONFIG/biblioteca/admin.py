b101873fd5601ae2f91b6a25dc6cb5b1f3d80e2d
unknown <diogenespuig@gmail.com>
2020-05-14 14:19:42 -0300

    se agregaron mas clases

--- CONFIG/biblioteca/admin.py
+++ CONFIG/biblioteca/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from biblioteca.models import Autor,Libro,Ejemplar,Usuario
 
 # Register your models here.
--- CONFIG/biblioteca/models.py
+++ CONFIG/biblioteca/models.py
@@ -6,19 +6,40 @@ class Autor(models.Model):
     Codigo = models.AutoField(primary_key=True)
     Nombre = models.CharField(max_length=50)
 
-class Libro(object):
+    def __str__(self):
+        return '{}'.format(self.Nombre,self.Codigo)
+
+class Libro(models.Model):
     Codigo = models.AutoField(primary_key=True)
     Titulo = models.CharField(max_length=50)
     Editorial = models.CharField(max_length=50)
-    Paginas = models.BigIntegerField()
-    Autor = Autor()
+    Paginas = models.IntegerField()
+    Autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}'.format(self.Codigo,self.Titulo,self.Editorial,self.Autor,self.Paginas)
 
 class Ejemplar(models.Model):
     Codigo = models.AutoField(primary_key=True)
     Localizacion = models.CharField(max_length=50)
+    Libro = models.ForeignKey(Libro, on_delete=models.CASCADE, default="")
+
+    def __str__(self):
+        return '{}'.format(self.Codigo,self.Localizacion,self.Libro)
 
 class Usuario(models.Model):
     Codigo = models.AutoField(primary_key=True)
     Nombre = models.CharField(max_length=50)
     Telefono = models.CharField(max_length=50)
     Direccion = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '{}'.format(self.Codigo,self.Nombre,self.Telefono,self.Direccion)
+
+class Ejem_Usua(models.Model):
+    id = models.AutoField(primary_key=True)
+    Ejemplar = models.ForeignKey(Ejemplar, on_delete=models.CASCADE)
+    Usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}'.format(self.id,self.Ejemplar,self.Usuario)
