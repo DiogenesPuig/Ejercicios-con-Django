6f3cd9535094703d09e34966416a25ef8f21235b
unknown <diogenespuig@gmail.com>
2020-05-14 13:22:38 -0300

    se crearon las clases que se necesitan y se creo la base de datos.sqlite3. Ademas se agrego la aplicacion a settings.py

--- CONFIG/biblioteca/models.py
+++ CONFIG/biblioteca/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Autor(models.Model):
+    Codigo = models.AutoField(primary_key=True)
+    Nombre = models.CharField(max_length=50)
+
+class Libro(object):
+    Codigo = models.AutoField(primary_key=True)
+    Titulo = models.CharField(max_length=50)
+    Editorial = models.CharField(max_length=50)
+    Paginas = models.BigIntegerField()
+    Autor = Autor()
+
+class Ejemplar(models.Model):
+    Codigo = models.AutoField(primary_key=True)
+    Localizacion = models.CharField(max_length=50)
+
+class Usuario(models.Model):
+    Codigo = models.AutoField(primary_key=True)
+    Nombre = models.CharField(max_length=50)
+    Telefono = models.CharField(max_length=50)
+    Direccion = models.CharField(max_length=50)
