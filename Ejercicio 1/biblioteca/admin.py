30aa84b813c6989c9eac47583ffdbc5b46818e17
unknown <diogenespuig@gmail.com>
2020-06-24 16:14:25 -0300

    Se hizo el punto 2

--- Ejercicio 1/biblioteca/admin.py
+++ Ejercicio 1/biblioteca/admin.py
@@ -7,10 +7,20 @@ from biblioteca.models import Usuario
 class LibroAdmin(admin.ModelAdmin):
     list_display = ('Titulo','Editorial')
 
+class UsuarioAdmin(admin.ModelAdmin):
+    fieldsets =(
+    ('Datos',{
+        'fields': ('Nombre',)
+    }),
+    ('Contacto',{
+        'fields': ('Telefono','Direccion')
+    })
+    )
+
 
 
 
 admin.site.register(Autor,)
 admin.site.register(Libro,LibroAdmin)
 admin.site.register(Ejemplar,)
-admin.site.register(Usuario,)
+admin.site.register(Usuario,UsuarioAdmin)
